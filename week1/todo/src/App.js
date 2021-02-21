2bc1ef747b78b3b5f80ffc1d0f9ebb7ba9f941e3
MelissaHoffman09 <melissa.hoffman@bryanuniversity.edu>
2021-02-20 17:19:41 -0800

    first

---
+++ week1/todo/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+  return (
+    <div>
+      <p>Things to do today</p>
+      <input type="checkbox" />Work Out<br/>
+      <input type="checkbox" />Do Homework<br/>
+      <input type="checkbox" />Pay Bills<br/>
+    </div>
+  );
+}
+
+export default App;
